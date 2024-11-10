8aabb58016725b41cfbfae54005f4a909396582f
Ahmed Hegazy <ahmedhegz96@gmail.com>
2024-11-10 22:51:04 +0200

    msg

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Services from "./Services";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
 import Navbar from "./components/Navbar";
-import Contact from "./Contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import "./App.css";
 
