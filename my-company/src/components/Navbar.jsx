fa497f9b68a29346110b26f5636f964610ef8008
Ahmed Hegazy <ahmedhegz96@gmail.com>
2024-11-10 22:57:25 +0200

    final project

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,32 +1,61 @@
+// src/components/Navbar.jsx
+import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  const navbarStyle = {
-    backgroundColor: "#333",
-    color: "#fff",
-    padding: "10px 0",
-    textAlign: "center",
-  };
-
-  const linkStyle = {
-    margin: "0 15px",
-    color: "#fff",
-    textDecoration: "none",
-    fontSize: "18px",
-  };
-
   return (
-    <div style={navbarStyle}>
-      <Link to="/" style={linkStyle}>
+    <div
+      style={{
+        backgroundColor: "#333",
+        color: "#fff",
+        padding: "10px 0",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link
+        to="/"
+        style={{
+          margin: "0 15px",
+          color: "#fff",
+          textDecoration: "none",
+          fontSize: "18px",
+        }}
+      >
         Home
       </Link>
-      <Link to="/about" style={linkStyle}>
+      <Link
+        to="/about"
+        style={{
+          margin: "0 15px",
+          color: "#fff",
+          textDecoration: "none",
+          fontSize: "18px",
+        }}
+      >
         About
       </Link>
-      <Link to="/services" style={linkStyle}>
+      <Link
+        to="/services"
+        style={{
+          margin: "0 15px",
+          color: "#fff",
+          textDecoration: "none",
+          fontSize: "18px",
+        }}
+      >
         Services
       </Link>
-      <Link to="/contact" style={linkStyle}>
+      <Link
+        to="/contact"
+        style={{
+          margin: "0 15px",
+          color: "#fff",
+          textDecoration: "none",
+          fontSize: "18px",
+        }}
+      >
         Contact
       </Link>
     </div>
