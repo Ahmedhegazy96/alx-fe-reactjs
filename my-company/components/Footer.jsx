4556013e2d903a752c6301ce7f41db27a68b29b2
Ahmed Hegazy <ahmedhegz96@gmail.com>
2024-11-10 22:54:42 +0200

    final project

---
+++ my-company/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "20px",
+        backgroundColor: "#333",
+        color: "#fff",
+      }}
+    >
+      <p>&copy; 2024 My Company. All rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
